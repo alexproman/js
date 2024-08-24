d36942b9e0f6f0c643f8b960c3c1ee464e7de4ed
Mohamed-hamid <bmwz4x3@gmail.com>
2024-08-24 17:12:05 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+  
+  console.log("%c  Mohamed HaMiiD    %c  Console  ","background-color:gold;color:red;font-size:14px;border:2px solid red;border-right:none","color:gold;font-size:14px;background:red;border:2px solid gold;border-left:none;")
+
+  let a = "Elzero Web School";
+
+  console.log(`${a.charAt(2).toUpperCase()}${a.slice(3, 6)}`);  //output:Zero
+  console.log("H".repeat(8));  //output:HHHHHHHH
+  console.log(a.split(" ", 1));  //output:[Elzeroo]
+  console.log(`${a.substr(0, 6)} ${a.substr(11, 6)}`);  //output:Elzero School
+  
+  console.log(
+    `${a.charAt(0).toLowerCase()}${a.slice(1, -1).toUpperCase()}${a.slice(-1).toLowerCase()}`);//output:eLZEROO WEB SCHOOl
+  
+
+
+
+ 
